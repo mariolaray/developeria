64c7f517fae58d7ec7276076ba7e70bf7a15e059
MARIO HERRANZ LARAY <mario.laray@gmail.com>
2024-09-11 11:36:17 +0200

    Ejercicios hasta 1.2.2

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,6 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
 
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
+//EJERCICIO 1.1.1
+//Console.Write("Congratulations!");
+//Console.Write(" ");
+//Console.Write("You wrote your first lines of code.");
+
+//EJERCICIO 1.1.2
+//Console.WriteLine("This is the first line");
+//Console.WriteLine("");
+//Console.WriteLine("This is the second line");
+
+//EJERCICIO 1.1.3
+//Console.WriteLine('b');
+/*
+
+// EJERCICIO 1.2.1
+string firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Berta";
+Console.WriteLine(firstName);
+firstName = "Josefa";
+Console.WriteLine(firstName);
+firstName = "Raul";
+Console.WriteLine(firstName);
+
+// EJERCICIO 1.2.2
+string name = "Bob";
+int num1 = 3;
+double num2 = 34.4;
+
+Console.Write("Hello " + name + "! You have " + num1 + " messages in your inbox. The temperature is "+num2 + " celsius.");
+*/
