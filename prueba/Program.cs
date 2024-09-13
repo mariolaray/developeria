d752c9053800ae5f7c55307bf6ad973aa0eab496
MARIO HERRANZ LARAY <mario.laray@gmail.com>
2024-09-13 13:34:11 +0200

    Ejemplo if no resuelto 2.3

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,60 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
 /*
-//EJEMPLOS 1.4 (Sumas con conversion implicita de datos)
-int firstNumber = 12;
-int secondNumber = 7;
-Console.WriteLine(firstNumber + secondNumber);
+// EJEMPLO DE UN RANDOM 2.2
+Random dice = new Random();
+int roll = dice.Next(1,7);
+Console.WriteLine(roll);
 
-string firstName = "Bob";
-int widgetsSold = 7;
-Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+// EJERCICIO 2.2.5
+int firstValue = 500;
+int secondValue = 600;
+int max = Math.Max(firstValue,secondValue);
 
-
-// EJERCICIO PROPUESTO 1 calculadora
-int num1 = 7;
-int num2= 2;
-
-int resSuma = num1+num2;
-int resResta = num1-num2;
-int resMul = num1*num2;
-int resDiv = num1/num2;
-
-Console.WriteLine("Suma " + resSuma + " Resta " + resResta + " Multiplicacion "
-+ resMul + " Division " + resDiv );
-
-// EJERCICIO PROPUESTO 2 Division usando datos decimales literales
-
-double div1 = 15.15;
-double div2 = 5;
-
-double divRes = div1/div2;
-Console.WriteLine("Division decimales : " + divRes);
-
-// EJERCICIO PROPUESTO 3 Division convertiendo a enteros
-
-double resConvers = (double)num1/num2;
-Console.WriteLine("Conversion divisiona enteros es " + resConvers);
-
-// EJERCICIO PROPUESTO 4 Resto division entera
-
-int resto = num1%num2;
-
-Console.WriteLine("El resto es " + resto);
-
-
-//EJERCICIO 1.4.2 Prueba de celsius
-int fahrenheit = 94;
-decimal celsius = (fahrenheit - 32m) * (5m / 9m);
-Console.WriteLine("The temperature is " + Math.Round(celsius,1) + " Celsius.");
-
-
-int notSophia = 93;int notSophia2 = 87;int notSophia3 = 98;int notSophia4 = 95;int notSophia5 = 100;
-int notNicolas =80;int notNicolas2 = 83;int notNicolas3 = 82; int notNicolas4=88;int notNicolas5 = 85;
-
-double hola = 30.1415;
-Console.WriteLine(hola.ToString("F1"));
-Type tipoHola = hola.GetType();
-Console.WriteLine($"El tipo de variable es : {tipoHola}");
-
+Console.WriteLine(max);
 */
+// EJEMPLO DE 2.3.2 IF'S
+Random dice = new Random();
+
+var total = 1;
+var tirada = 1;
+
+Console.Write("Dice roll");
+for (int i = 0; i < 3; i++)
+{
+    tirada = dice.Next(1, 7);
+    Console.Write($" {tirada} + ");
+    total += tirada;
+}
+
+
+Console.WriteLine($"Total: {total}");
+
+if (total > 10)
+{
+    Console.WriteLine("You win!");
+}
+
+else
+{
+    Console.WriteLine("Sorry, you lose.");
+}
