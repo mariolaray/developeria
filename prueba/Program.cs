8b5d7de3ff00ad076d0a79b6f32495be311cfd97
MARIO HERRANZ LARAY <mario.laray@gmail.com>
2024-09-17 11:15:10 +0200

    Ejercicio 2.6 arrays y bucles resuelto

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -12,7 +12,7 @@ int secondValue = 600;
 int max = Math.Max(firstValue,secondValue);
 
 Console.WriteLine(max);
-*/
+
 // EJEMPLO DE 2.3.2 IF'S
 Random dice = new Random();
 
@@ -39,3 +39,73 @@ else
 {
     Console.WriteLine("Sorry, you lose.");
 }
+*/
+//EJERCICIO DE MEDIAS CON ARRAYS 
+// Crear un array de nombres
+        string[] nombres = {"Sophia", "Andrew", "Emma", "Logan"};
+
+        // Crear un array de enteros
+        int[][] notas = new int[4][];
+        notas[0] = new int[] {90, 86, 87, 98, 100};
+        notas[1] = new int[] {92, 89, 81, 96, 90};
+        notas[2] = new int[] {90, 85, 87, 98, 68};
+        notas[3] = new int[] {90, 95, 87, 88, 96};
+
+        var indiceDeNota = 0;
+string letra = "";
+
+        // Mostrar los nombres y sus valores asociados
+        foreach (var nombre in nombres)
+        {
+            int sumaDeValores = 0;
+            
+            foreach (var nota in notas[indiceDeNota])
+            {
+                sumaDeValores += nota;
+            }
+
+            var media = sumaDeValores / notas[indiceDeNota].Length;
+            
+            if (media >= 97)
+        letra = "A+";
+
+    else if (media >= 93)
+        letra = "A";
+
+    else if (media >= 90)
+        letra = "A-";
+
+    else if (media >= 87)
+        letra = "B+";
+
+    else if (media >= 83)
+        letra = "B";
+
+    else if (media >= 80)
+        letra = "B-";
+
+    else if (media >= 77)
+        letra = "C+";
+
+    else if (media >= 73)
+        letra = "C";
+
+    else if (media >= 70)
+        letra = "C-";
+
+    else if (media >= 67)
+        letra = "D+";
+
+    else if (media >= 63)
+        letra = "D";
+
+    else if (media >= 60)
+        letra = "D-";
+
+    else
+        letra = "F";
+
+            Console.WriteLine($"Nombre: {nombre}, Valores: {string.Join(",", notas[indiceDeNota])}, Media: {media} , Letra: {letra}");
+
+            indiceDeNota++;
+        }
