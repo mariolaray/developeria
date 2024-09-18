87e1bc346d8b03cb3686489c472872b7d2726cba
MARIO HERRANZ LARAY <mario.laray@gmail.com>
2024-09-18 13:24:28 +0200

    Ejercicio 3.5 salud heroe y monstruo

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,26 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
 /*
+Random random = new Random();
+int current = 0;
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
 */
 
-// EJERCICIO 3.4 mostrar resto 0 con un for e if de 3 , 5 y 3&5 del 1 al 100
-for (int i = 0; i < 101; i++)
-{
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-        Console.WriteLine(i + "-FizzBuzz");
-    }
-    else if (i % 3 == 0)
-    {
-        Console.WriteLine(i + "-Fizz");
-    }
-    else if (i % 5 == 0)
-    {
-        Console.WriteLine(i + "-Buzz");
-    }
-    else
-    {
-        Console.WriteLine(i);
-    }
+// 
+Random random = new Random();
 
-}
+int saludMonstruo = 10;
+int saludHeroe = 10;
+int daño;
+
+do
+{
+    
+    if (saludMonstruo > 0)
+    {
+        daño = random.Next(0, 11);
+        saludMonstruo -= daño;
+        Console.WriteLine($"DAÑO INFLINGIDO AL MONSTRUO : {daño}* , le queda al monstruo: {saludMonstruo} de vida");
+        if (saludMonstruo <= 0)
+        {
+            saludMonstruo = 0;
+            Console.WriteLine($"LA SALUD DEL MONSTRUO HA LLEGADO A {saludMonstruo} Ó MENOS, el monstruo ha sido derrotado");
+        }
+
+
+        if (saludHeroe > 0 && saludMonstruo > 0)
+        {
+            daño = random.Next(1, 11);
+            saludHeroe -= daño;
+            Console.WriteLine($"DAÑO INFLINGIDO AL HEROE : {daño}* , le queda al heroe: {saludHeroe} de vida");
+        }
+        if (saludHeroe <= 0)
+        {
+            saludHeroe = 0;
+            Console.WriteLine($"LA SALUD DEL HEROE HA LLEGADO A {saludHeroe} Ó MENOS , el heroe ha sido derrotado");
+        }
+    }
+} while (saludMonstruo > 0 && saludHeroe > 0);
+
