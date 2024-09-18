3c977384710932958ec24c5370f154cd31328de8
MARIO HERRANZ LARAY <mario.laray@gmail.com>
2024-09-18 11:41:24 +0200

    Ejercicio 3.4 resto % con for e if hasta 100

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,34 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
 /*
-//Ejemplo 3.1 Caso ternario < >
-
-int saleAmount = 1001;
-// int discount = saleAmount > 1000 ? 100 : 50;
-
-Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
-
-//Ejercicio 3.1 Bucle con ternario que dice si es cara o cruz
-
-Random n1 = new Random();
-
-while(true){
-    int randomN = n1.Next(0,2);
-    Console.WriteLine((randomN == 0) ? "heads": "tails");
-    System.Threading.Thread.Sleep(500);
-}
 */
-// EJERCICIO 3.1.2 Ejercicio con tenario con privliegios
-string permission = "Admin";
-int level = 15;
-        // Determinar el mensaje usando expresiones ternarias
-        string message = permission.Contains("Admin")
-            ? (level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.")
-            : permission.Contains("Manager")
-                ? (level > 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.")
-                : "You do not have sufficient privileges.";
 
-        // Mostrar el mensaje
-        Console.WriteLine(message);
+// EJERCICIO 3.4 mostrar resto 0 con un for e if de 3 , 5 y 3&5 del 1 al 100
+for (int i = 0; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine(i + "-FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine(i + "-Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(i + "-Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
 
-
+}
